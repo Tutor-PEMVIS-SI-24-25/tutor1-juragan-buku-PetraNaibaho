fec2f911d54ea914d40153bbc1a761407af808b4
PetraNaibaho <petranaibaho5@gmail.com>
2024-09-04 21:27:19 +0700

    Telah Selesai

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
+//12S24002 - Petra Ignatius Pengayoman Naibaho
 // NIM - NAMA
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int nama;
+
+        nama = input.nextInt();
+        System.out.println("Petra");
+    }
+}
